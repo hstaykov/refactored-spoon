2f6af074786880fc1610f4c4b43c50f5360f566f
Hristo Staykov <riceto.new@gmail.com>
2019-09-14 11:22:32 +0300

    Update just_scripts.js

--- just_scripts.js
+++ just_scripts.js
@@ -1,15 +1,21 @@
-(function recruit(i) {
+//Units
+//Spy : spies/KS
+//Archer : archer/S1 S2 S3
+//Infantry : infantry/M1 M2 M3
+//Spear : infantry/P1 P2 P3
+//Cavalery : cavalery/K1 K2 K3
+
+//Change function to xajax_doUpgrade for upgrading
+
+(function myLoop(i) {
   setTimeout(function() {
     xajax_doSwitchProvince({
       to: 'next'
     });
-    
-    // unit Spy
-    // amount amount
     xajax_doRecruit('spies', {
       unit: 'KS',
       amount: 2805
     })
-    if (--i) recruit(i);
+    if (--i) myLoop(i);
   }, 5000)
-})(5);// number of times
+})(5);
